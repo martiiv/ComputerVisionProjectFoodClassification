558b89915bc6e559b1543a15777ab14434fef391
Martin Iversen <Martin>
2021-04-19 15:17:59 +0200

    Created test script and added in more images for testing

--- TestingFunctions.m
+++
@@ -1,9 +0,0 @@
-%%This matlab script will be used for experimenting with food
-%%classification functions
-
-image = imread('images/ChickenSalad.jpg');
-
-green = Green(image);
-red = Red(image);
-
-imshow(red)
