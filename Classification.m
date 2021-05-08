58c3006b57f35a14c58e3708d889d53f392121e2
Martin Iversen <Martin>
2021-05-08 13:48:44 +0200

    Final code revision, renamed main livescript to main and commented code

--- Classification.m
+++ Classification.m
@@ -1,8 +1,16 @@
 function[image] = Classification(image)
+%%Project food classification script
+%%This will contain the actual code we will use
+%%Martin Iversen 
+%%Last revision: 08.05.2021
 
+%%Reads the image
+image = imread(image);
+
+%%Shows original image
 imshow(image)
 
-%%Defines the different binary masks for the images based on color
+%%Runs the different color functions for the image 
 green = Green(image);        
 lightGreen = LightGreen(image);
 red = MoreRed(image);
@@ -13,16 +21,17 @@ orange = Orange(image);
 %%Fuses both green masks to cover as much green as possible
 greenResult = imfuse(lightGreen,green,'blend');
 
-%%Fuses all of the masks together with diff 
+%%Fuses all of the masks together with diff method
 result = imfuse(greenResult, red,'diff');
 result = imfuse(yellow,result,'diff');
 result = imfuse(purple,result,'diff');
 result = imfuse(orange, result,'diff');
 
+%%Shows resulting image pre post-processing
 imshow(result)
 
-%%Defines a morphological structuring object
-rough = strel('diamond',6);
+%%Defines a morphological structuring object with size 5
+rough = strel('diamond',5);
 
 %%Perform morphological opening on the image
 roughProcessed = imopen(result,rough);
@@ -30,6 +39,7 @@ imshow(roughProcessed)
 
 fused = imfuse(image,roughProcessed,'blend');
 
-%%Display the images
+%%Display the result
 imshow(fused)
+
 
